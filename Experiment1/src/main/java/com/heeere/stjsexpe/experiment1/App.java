bece5b01e8da528d96b16a8ccecb6661e2c8cadc
Rémi Emonet <remi.emonet@reverse--com.heeere>
2013-10-29 11:38:20 +0100

    added stjs plugin and dependency (including DOM bridge)

--- Experiment1/src/main/java/com/heeere/stjsexpe/experiment1/App.java
+++ Experiment1/src/main/java/com/heeere/stjsexpe/experiment1/App.java
@@ -6,8 +6,6 @@ package com.heeere.stjsexpe.experiment1;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
     }
 }
