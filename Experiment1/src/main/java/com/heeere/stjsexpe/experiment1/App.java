e52d8a810aceff0028f9234be769f5383be28406
Rémi Emonet <remi.emonet@reverse--com.heeere>
2013-10-29 11:48:11 +0100

    simple html page with an alert onload... that works!

--- Experiment1/src/main/java/com/heeere/stjsexpe/experiment1/App.java
+++ Experiment1/src/main/java/com/heeere/stjsexpe/experiment1/App.java
@@ -1,11 +1,18 @@
 package com.heeere.stjsexpe.experiment1;
 
-/**
- * Hello world!
- *
- */
+import static org.stjs.javascript.Global.*;
+import org.stjs.javascript.dom.DOMEvent;
+import org.stjs.javascript.functions.Callback1;
+
+
 public class App 
 {
     public static void main( String[] args ) {
+        window.onload = new Callback1<DOMEvent>() {
+            @Override
+            public void $invoke(DOMEvent p1) {
+                alert("Test");
+            }
+        };
     }
 }
