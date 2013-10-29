62e65a8105ce61b15b6bf8f9b3a46a0d5afda47b
Rémi Emonet <remi.emonet@reverse--com.heeere>
2013-10-29 11:54:57 +0100

    now adds a callback to the button

--- Experiment1/src/main/java/com/heeere/stjsexpe/experiment1/App.java
+++ Experiment1/src/main/java/com/heeere/stjsexpe/experiment1/App.java
@@ -2,7 +2,10 @@ package com.heeere.stjsexpe.experiment1;
 
 import static org.stjs.javascript.Global.*;
 import org.stjs.javascript.dom.DOMEvent;
+import org.stjs.javascript.dom.Element;
+import org.stjs.javascript.dom.Input;
 import org.stjs.javascript.functions.Callback1;
+import org.stjs.javascript.functions.Function1;
 
 
 public class App 
@@ -11,7 +14,17 @@ public class App
         window.onload = new Callback1<DOMEvent>() {
             @Override
             public void $invoke(DOMEvent p1) {
-                alert("Test");
+                alert("Hello onload");
+                final Element button = window.document.getElementById("the-button");
+                final Input textfield = (Input) window.document.getElementById("the-textfield");
+                
+                button.onclick = new Function1<DOMEvent, Boolean>() {
+                    @Override
+                    public Boolean $invoke(DOMEvent p1) {
+                        alert("Hello \""+textfield.value+"\"");
+                        return true;
+                    }
+                };
             }
         };
     }
